508e9c4a2fe5b9716f10bbd337cb941b9a18f6bf
Veljko-Laces <veljkolaces123@gmail.com>
2025-01-31 00:02:49 +0100

    mise en place des variables et implémentation de la section Accueil

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <a href="#competences">Compétences</a>
+        <a href="#projets">Projets</a>
+        <a href="#contact">Contact</a>
+        <img src="./assets/images/svg/lune.svg" alt="" class="icones" id="change-mode">
+    </nav>
+    <div class="accueil">
+        <img src="./assets/images/svg/logo-mobile.svg " class="logo-desktop logo-mobile icones " alt="">
+        <div class="accueil-contenu">
+            <p class="text-lg">Bonjour, je suis</p>
+            <div class="accueil-text">
+                <h1>VELJKO LACES</h1>
+                <p class="text-lg"><span class="bold">Ingénieur full-stack</span> basé à Paris, engagé dans la conception d’expériences numériques d’exception.</p>
+            </div>
+            <div class="accueil-btns">
+                <button class="accueil-btn">
+                    <img src="./assets/images/svg/whatsapp.svg" alt="" class="icones"> 
+                    <p class="TEXT-base-boutton">Whatsapp</p>
+                </button>
+                <button class="accueil-btn">
+                    <img src="./assets/images/svg/linkedin.svg" class="icones" alt="">
+                    <p class="TEXT-base-boutton">Linkedin</p>
+                </button>
+                <button class="accueil-btn">
+                    <img src="./assets/images/svg/mail.svg" alt="" class="icones">
+                    <p class="TEXT-base-boutton">veljkolaces123@gmail.com</p>
+                </button>
+                <button class="accueil-btn">
+                    <img src="./assets/images/svg/github.svg" alt="" class="icones">
+                    <p class="TEXT-base-boutton">Github</p>
+                </button>
+            </div> 
+        </div>
+    </div>
