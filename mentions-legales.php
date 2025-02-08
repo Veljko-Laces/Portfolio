ef7d40a5f75f9dcb5dcc3e97eb0477121fa2ad0e
Veljko-Laces <veljkolaces123@gmail.com>
2025-02-08 12:36:46 +0100

    implémentation de la page des mentions légales et ajout d'un boutton pour le CV

---
+++ mentions-legales.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mentions légales</title>
+    <link rel="stylesheet" href="assets/css/mentions-legales.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <a href="index.php#competences">Compétences</a>
+        <a href="index.php#projets">Projets</a>
+        <a href="index.php#contact">Contact</a>
+        <img src="./assets/images/svg/lune.svg" alt="" class="icones" id="change-mode">
+    </nav>
+    <div class="mentions-legales-container">
+        <div class="titre-page-mentions-legales">
+            <button class="accueil-btn">
+                <a href="./index.php" class="TEXT-base-boutton a-accueil-contact">Revenir a l'acceuil</a>
+            </button>
+            <h1 class="titre-mentions">Mentions légales</h1>
+        </div>
+        <div class="contenue-mentions-legales">
+            <div class="blocs">
+                <p class="text-lg">Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique, il est précisé aux utilisateurs du site veljko-laces.fr l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.</p>
+                <h2 class="titre">Edition du site </h2>
+                <p class="text-lg">Le présent site, accessible à l’URL https://veljko-laces.fr (le « Site »), est édité par :</p>
+                <p class="text-lg">Veljko Laces, résidant 8 rue jean renoir 93130 Noisy-le-Sec, de nationalité Française (France), né(e) le 21/10/2006,</p>
+            </div>
+            <div class="blocs">
+                <h2 class="titre">Hébergement</h2>
+                <p class="text-lg">Le Site est hébergé par la société OVH SAS, situé 2 rue Kellermann - BP 80157 - 59053 Roubaix Cedex 1, (contact téléphonique ou email : 1007).</p>
+            </div>
+            <div class="blocs">
+                <h2 class="titre">Directeur de publication </h2>
+                <p class="text-lg">Le Directeur de la publication du Site est Veljko Laces.</p>
+            </div>
+            <div class="blocs">
+                <h2 class="titre">Me contacter</h2>
+                <p class="text-lg">Par téléphone : +33769117472</p>
+                <p class="text-lg">Par email : veljkolaces123@gmail.com</p>
+                <p class="text-lg">Par courrier : 8 rue jean renoir 93130 Noisy-le-Sec</p>
+            </div>
+        </div>
+    </div>
+    <script src="./assets/js/app.js"></script>
+</body>
+</html>
