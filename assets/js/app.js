f13fedf4213d354ef1eb04255858078810e7a5bf
Veljko-Laces <veljkolaces123@gmail.com>
2025-02-12 19:09:04 +0100

    Ajout des messages d'erreurs et de succès pour le formulaires

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,80 @@ changeColorBtn.addEventListener('click', function() {
     }
 
 });
+
+
+function nomLength() {
+    let nom = document.getElementById("add-name").value
+    const nomErreur = document.getElementById('nom-erreur')
+    
+    if (nom === "") {
+        nomErreur.innerHTML = `
+        <div class="erreur">
+            <p class="text-base">Veuillez entrer un nom.</p>
+        </div>
+        `;
+        return;
+    }
+}
+
+function emailLength() {
+    let email = document.getElementById("add-email").value
+    const emailErreur = document.getElementById('email-erreur')
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/
+    const isEmailValid = regex.test(email)
+    
+    if (!isEmailValid) {
+        emailErreur.innerHTML = `
+        <div class="erreur">
+            <p class="text-base">Veuillez entrer un email.</p>
+        </div>
+        `;
+        return;
+    }
+}
+
+function messageLength() {
+    let message = document.getElementById("add-message").value
+    const messageErreur  = document.getElementById('message-erreur')
+    
+    if (message === "") {
+        messageErreur .innerHTML = `
+        <div class="erreur">
+            <p class="text-base">Veuillez écrire un message.</p>
+        </div>
+        `;
+        return;
+    }
+}
+
+
+function envoyeForm() {
+    const formulaireEnvoye = document.getElementById('formulaire-envoye')
+
+    formulaireEnvoye.innerHTML = ''
+
+    const nomOk = nomLength()
+    const emailOk = emailLength()
+    const msgOk = messageLength()
+
+
+    if (nomOk && emailOk && msgOk) {
+        document.getElementById("add-name").value = ''
+        document.getElementById('add-email').value = ''
+        document.getElementById('add-message').value = ''
+        
+
+        return formulaireEnvoye.innerHTML =`
+        <div class="succes">
+            <p class="text-base">Le formulaire a bien été envoyé.</p>
+        </div>
+        `
+    }
+}
+
+const monButton = document.getElementById('send-form')
+
+monButton.addEventListener('click', () => {
+    envoyeForm();
+})
+
